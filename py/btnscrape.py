f8b4dde1a24e28c5c12ef6332fe2c5b8c1d7f80b
Jeff DeButte <jeff@inharmonic.com>
2013-03-06 16:32:49 -0500

    Updating scrape script, outputs to JSON object (not yet checked for compatibility with D3 in terms of format)

--- py/btnscrape.py
+++ py/btnscrape.py
@@ -5,11 +5,9 @@ from BeautifulSoup import BeautifulSoup
 soup = BeautifulSoup(urllib2.urlopen('http://www.behindthenet.ca/nhl_statistics.php?c=0+1+3+5+4+6+7+8+13+14+29+30+32+33+34+45+46+63+67&ds=30&f5=#snip=f').read())
 
 headersScrape = soup.find("tr", { "class" : "col-title-row" }).findAll("td")
-cols = 0
 headers = [];
 for col in headersScrape:
 	headers.append(col['class']);
-	cols += 1
  
 playersScrape = soup.find("tbody", {"class" : "players"}).findAll("tr")
 counter = 0
@@ -18,12 +16,13 @@ for playerScrape in playersScrape:
 	tds = playerScrape.findAll("td")
 	player = {}
 	for td in tds:
-		#print td.contents[0], " ", counter
-		player[headers[counter]] = td.contents[0]
+		player[headers[counter]] = str(td.contents[0]).replace("\"", "")
 		counter += 1
 		if (counter == 20):
 			players.append(player)
 			counter = 0
 			
-for playerPrint in players:
-  print playerPrint	
+#for playerPrint in players:
+#  print playerPrint	
+
+json.dump(players, open("../json/5v5.json",'w'))
